4be11936d44203ecc598ed5c93b72f902d7ac645
Shrimadh <shrimadh332001@gmail.com>
2024-10-25 01:55:57 -0400

    Updated ajax search link

--- src/recommenderapp/static/script.js
+++ src/recommenderapp/static/script.js
@@ -10,7 +10,7 @@ $(document).ready(function () {
 			source: function (request, response) {
 				$.ajax({
 					type: "POST",
-					url: "http://localhost:5000/search",
+					url: "http://127.0.0.1:5001/search",
 					dataType: "json",
 					cache: false,
 					data: {
