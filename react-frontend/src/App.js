66ec6be2a389d50274db64a9e2a8d3f333e2b713
Akul G Devali <64837282+akuldevali@users.noreply.github.com>
2024-10-31 08:30:08 +0000

    Configured route for /wall

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,14 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Wall from './components/Wall';
 import React, {useState, useEffect} from 'react';
 
 
 function App() {
   return (
-    <div className="App">
-      <Wall />
-    </div>
+      <Router>
+      <Routes>
+        <Route path="/wall" element={<Wall />} />
+      </Routes>
+    </Router>
   );
 }
 
