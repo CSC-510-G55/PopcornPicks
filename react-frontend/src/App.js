c4bc3d8d3fc3d0efd8919abbfdff0c37895ca051
Akul G Devali <64837282+akuldevali@users.noreply.github.com>
2024-10-31 08:05:56 +0000

    Created React component for Wall

---
+++ react-frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import Wall from './components/Wall';
+import React, {useState, useEffect} from 'react';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Wall />
+    </div>
+  );
+}
+
+export default App;
