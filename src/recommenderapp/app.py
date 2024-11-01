bf7ceec180b86982171b8bc081812a359e0b04cc
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-10-31 22:17:48 -0400

    Harcoded for testing

--- src/recommenderapp/app.py
+++ src/recommenderapp/app.py
@@ -48,7 +48,7 @@ db = client.PopcornPicksDB
 
 cors = CORS(app, resources={r"/*": {"origins": "*"}})
 user = {1: None}
-# user[1] = "671b289a193d2a9361ebf39a"  # Hardcoded user id for testing purposes
+user[1] = "671b289a193d2a9361ebf39a"  # Hardcoded user id for testing purposes
 
 movies_df = pd.read_csv("data/movies.csv")
 
