2bd26e5cbcfe7b9db13f3f491967649d22935634
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-11-01 16:32:09 -0400

    Black changes

--- set_env.py
+++ set_env.py
@@ -6,7 +6,7 @@ env_variables = {
     "MONGO_URI": "mongodb+srv://svrao3:popcorn1234@popcorn.xujnm.mongodb.net",
 }
 
-with open(".env", "w",encoding="utf-8") as env_file:
+with open(".env", "w", encoding="utf-8") as env_file:
     for key, value in env_variables.items():
         env_file.write(f"{key}={value}\n")
 
