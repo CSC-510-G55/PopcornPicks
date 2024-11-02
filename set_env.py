7078b50eca320c9c90bfd4df7f60d3318ac2295f
Srimadh V Rao <64469917+Shrimadh@users.noreply.github.com>
2024-11-02 02:18:40 -0400

    Changed env

--- set_env.py
+++ set_env.py
@@ -5,8 +5,7 @@ This script will create a .env file with environment variables.
 env_variables = {
     "MONGO_URI": "mongodb+srv://svrao3:popcorn1234@popcorn.xujnm.mongodb.net",
     "APP_PASSWORD": "tnjydyefhlpsmdao",
-    "REACT_APP_API_URL": "http://127.0.0.1:5000"
-
+    "REACT_APP_API_URL": "http://127.0.0.1:5000",
 }
 
 with open(".env", "w", encoding="utf-8") as env_file:
